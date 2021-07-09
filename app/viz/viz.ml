52a1f176d393f1d5f183b3720de770faf0df9f71
Alexandru Scvortov <code@scvalex.net>
2021-07-09 15:54:53 +0100

    Stub display

--- app/viz/viz.ml
+++ app/viz/viz.ml
@@ -2,11 +2,43 @@ open! Core
 module G = Graphics
 open Icfpc2021
 
+let draw_bg () =
+  G.clear_graph ();
+  G.set_color G.black;
+  let width = G.size_x () in
+  let height = G.size_y () in
+  G.fill_rect 0 0 width height;
+  G.moveto ((width / 2) - 50) (height - 20);
+  G.set_color G.white;
+  G.draw_string "ICFP 2021"
+;;
+
+let rec interact ~prob =
+  draw_bg ();
+  let status = G.wait_next_event [ G.Key_pressed ] in
+  match (status : G.status) with
+  | { keypressed = true; key = 'q' | '\027'; _ } -> print_endline "Quitting..."
+  | _ ->
+    printf
+      !"Ignoring input: %{sexp#mach: Sexp.t}\n%!"
+      [%sexp
+        { mouse_x : int = status.mouse_x
+        ; mouse_y : int = status.mouse_y
+        ; button : bool = status.button
+        ; keypressed : bool = status.keypressed
+        ; key : char = status.key
+        }];
+    let (_ : float) = Unix.nanosleep 0.033 in
+    interact ~prob
+;;
+
 let display ~filename =
   let prob = Problem.load_exn ~filename in
-  G.open_graph " 1000x1000";
+  G.open_graph " 1000x800";
   G.set_window_title "ICFPC 2021";
-  printf !"%{Problem#hum}" prob
+  printf !"%{Problem#hum}\n%!" prob;
+  let () = interact ~prob in
+  G.close_graph ()
 ;;
 
 let commands =
