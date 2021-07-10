17e7bae94a04f42dd7fefeaf786f200e85d83514
Thomas Braibant <thomas.braibant@gmail.com>
2021-07-10 12:04:55 +0100

    Fix geometry

--- lib/icfpc2021/segment.ml
+++ lib/icfpc2021/segment.ml
@@ -1,3 +1,5 @@
+open! Core
+
 type t =
   { a : Point.t
   ; b : Point.t
@@ -5,19 +7,6 @@ type t =
 [@@deriving fields, sexp]
 
 let create a b = Fields.create ~a ~b
-
-(* Find a,b,c such that ax + by = c *)
-let coefs { a; b } =
-  let open Bignum in
-  let x1 = a.x in
-  let y1 = a.y in
-  let x2 = b.x in
-  let y2 = b.y in
-  let a = y2 - y1 in
-  let b = x1 - x2 in
-  let c = (a * x1) + (b * y1) in
-  a, b, c
-;;
 
 (* Checkt that [point] belong to the segment, PROVIDED that we have checked that
    the three points are aligned. Scale is a hack to avoid having to deal with
@@ -34,20 +23,18 @@ let contains { a; b } scale (point : Point.t) =
   && point.y <= scale * maxy
 ;;
 
-let intersect s1 s2 =
+(* https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/ *)
+let ccw (a : Point.t) (b : Point.t) (c : Point.t) =
   let open Bignum in
-  let a1, b1, c1 = coefs s1 in
-  let a2, b2, c2 = coefs s2 in
-  let det = (a1 * b2) - (a2 * b1) in
-  if Bignum.equal det zero
-  then false (* Colinear *)
-  else (
-    (* x / det , y / det are the coordinates of the line intersection  *)
-    let x = (b2 * c1) - (b1 * c2) in
-    let y = (a1 * c2) - (a2 * c1) in
-    let p = Point.create ~x ~y in
-    (* Let's check that the intersection is on the first segment  *)
-    contains s1 det p && contains s2 det p)
+  (c.y - a.y) * (b.x - a.x) > (b.y - a.y) * (c.x - a.x)
+;;
+
+let intersect s1 s2 =
+  let a = s1.a in
+  let b = s1.b in
+  let c = s2.a in
+  let d = s2.b in
+  Bool.(ccw a c d <> ccw b c d) && Bool.(ccw a b c <> ccw a b d)
 ;;
 
 module Testing = struct
