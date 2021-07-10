999c1c4f567e3cfaea9ba036cff9388af145e641
Dmitry Astapov <dastapov@gmail.com>
2021-07-10 11:17:56 +0100

    Fix for contains

--- lib/icfpc2021/polygon.ml
+++ lib/icfpc2021/polygon.ml
@@ -18,17 +18,40 @@ https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
 ```
 *)
 
+(*
+Or, https://github.com/substack/point-in-polygon, which derives from the same source
+
+  var x = point[0], y = point[1];
+    
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
+        
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    
+    return inside;
+ *)
+
 let contains (t : t) (pt : Point.t) =
+  let x = pt.x in
+  let y = pt.y in
+  let c = ref false in
   let i = ref 0 in
   let j = ref (Array.length t - 1) in
-  let c = ref false in
   while !i < Array.length t do
     let open Bignum in
-    if ((t.(!i).y <= pt.y && pt.y < t.(!j).y) || (t.(!j).y <= pt.y && pt.y < t.(!i).y))
-       &&
-       let m = t.(!j).y - t.(!i).y in
-       m * (pt.x - t.(!i).x) < (t.(!j).x - t.(!i).x) * (pt.y - t.(!i).y)
-    then c := not !c;
+    let xi = t.(!i).x in
+    let yi = t.(!i).y in
+    let xj = t.(!j).x in
+    let yj = t.(!j).y in
+    let intersect =
+      Bool.( <> ) (yi > y) (yj > y) && x < ((xj - xi) * (y - yi) / (yj - yi)) + xi
+    in
+    if intersect then c := true;
     j := !i;
     i := Int.(!i + 1)
   done;
